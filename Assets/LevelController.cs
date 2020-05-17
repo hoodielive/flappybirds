0ddda5c6efb582d93712772e2dd5da9c48cd06dd
hoodielive <hoodielive@gmail.com>
2020-05-17 18:49:38 -0400

    Finished fat bird today

---
+++ Assets/LevelController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    private Enemy[] _enemies;
+
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+
+    void Update()
+    {
+        foreach (Enemy enemy in _enemies)
+        {
+            if (enemy != null)
+            {
+                return;
+            }
+
+            Debug.Log("You killed all your enemies!");
+        }
+    }
+}
